49acb22fda56aa92586ed6630fb2913f52a37369
Nikhil S Bharadwaj <nikhils.bharadwaj24@gmail.com>
2022-06-28 12:38:08 +0530

    Enable CORS + Schema validation

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const productRoute = require("./src/routes/productRoute.js");
 require("./src/db/dbconnection.js");
 
 const app = express();
 
-app.use(express.json());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(productRoute);
--- src/models/Products.js
+++ src/models/Products.js
@@ -1,10 +1,35 @@
 const mongoose = require("mongoose");
 
 const AppSchema = mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    price: { type: Number, required: true },
-    stock: { type: Number, required: true },
+    name: {
+        type: String,
+        required: true,
+        validate: {
+            validator: (value) => /^[a-zA-Z0-9_ ]*$/.test(value),
+            message: (prop) =>
+                `${prop} should contain only alphabets and digits`,
+        },
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+        validate: {
+            validator: (value) => value > 0,
+            message: (prop) => `${prop.path} should be greater than 0`,
+        },
+    },
+    stock: {
+        type: Number,
+        required: true,
+        validate: {
+            validator: (value) => value > 0,
+            message: (prop) => `${prop.path} should be greater than 0`,
+        },
+    },
 });
 
 const Products = mongoose.model("App", AppSchema);
